ebd9a85b8254332a3378e58d2a6cfe5d0f036af3
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-12-19 06:20:44 +0200

    1 upload router of product knife

--- app_knife/databases/database.py
+++ app_knife/databases/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = 'sqlite:///./src_knife/databases/market_knifes.db'
+DATABASE_URL = 'sqlite:///./app_knife/databases/market_knifes.db'
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
